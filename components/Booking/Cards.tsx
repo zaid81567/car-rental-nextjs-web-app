ffc2d5318b57f07c3780e29e4ea7d619a6126b56
zaid <zaid81567@gamil.com>
2024-05-25 08:32:02 +0530

    cards key={index}

--- components/Booking/Cards.tsx
+++ components/Booking/Cards.tsx
@@ -12,8 +12,11 @@ function Cards() {
       <div className="grid grid-cols-5 mt-2">
         {CardsList.map((item: any, index) => (
           <div
+            key={index}
             className={`w-[50px] border-[2px] flex items-center justify-center
-           rounded-md cursor-pointer hover:scale-110 hover:border-yellow-400 active:border-yellow-200 transition-all ${selectedCard == index?"border-yellow-400":null}`}
+           rounded-md cursor-pointer hover:scale-110 hover:border-yellow-400 active:border-yellow-200 transition-all ${
+             selectedCard == index ? "border-yellow-400" : null
+           }`}
             onClick={() => setSelectedCard(index)}
           >
             <Image src={item?.image} alt={item.name} width={30} height={50} />
